689625cdc2c003b29bb4c1d2d26c7fcf111f0ec6
Leonardo Pinheiro <leo.pinnheiro@gmail.com>
2020-08-12 04:39:28 -0300

    Validacao para não permitir transacao de saida caso nao possua saldo.

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,5 +1,5 @@
 import { getCustomRepository, getRepository } from 'typeorm';
-// import AppError from '../errors/AppError';
+import AppError from '../errors/AppError';
 
 import TransactionRepository from '../repositories/TransactionsRepository';
 
@@ -22,6 +22,14 @@ class CreateTransactionService {
   }: Request): Promise<Transaction> {
     const transactionsRepository = getCustomRepository(TransactionRepository);
     const categoryRepository = getRepository(Category);
+
+    // Verifica se possui saldo para fazer a transação
+    const { total } = await transactionsRepository.getBalance();
+    if (type === 'outcome' && total < value) {
+      throw new AppError(
+        'You do not have enough balance for this transaction.',
+      );
+    }
 
     // Verifico se a categoria existe.
     let transactionCategory = await categoryRepository.findOne({
